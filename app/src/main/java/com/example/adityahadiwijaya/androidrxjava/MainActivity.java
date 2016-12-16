f13f6cb538e035bf0a874532f2dbbe272d99de3a
Aditya Hadiwijaya <kulik2@ymail.com>
2016-12-16 16:34:03 +0700

    finish part 1 ui

--- app/src/main/java/com/example/adityahadiwijaya/androidrxjava/MainActivity.java
+++ app/src/main/java/com/example/adityahadiwijaya/androidrxjava/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.adityahadiwijaya.androidrxjava;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.adityahadiwijaya.androidrxjava.part1.Part1Activity;
+
+import rx.Observable;
+import rx.Subscriber;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wireUI();
+    }
+
+    private void wireUI(){
+        setTapListener(R.id.btnPart1);
+    }
+
+    private void setTapListener(int viewId){
+        findViewById(viewId).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        startActivity(getLaunchIntentFor(v.getId()));
+    }
+
+    private Intent getLaunchIntentFor(int viewId){
+        Intent launchIntent = null;
+
+        switch (viewId){
+            case R.id.btnPart1:
+                launchIntent = new Intent(this, Part1Activity.class);
+                break;
+        }
+
+
+        return launchIntent;
     }
 }
---
+++ app/src/main/java/com/example/adityahadiwijaya/androidrxjava/part1/Part1Activity.java
@@ -0,0 +1,20 @@
+package com.example.adityahadiwijaya.androidrxjava.part1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.adityahadiwijaya.androidrxjava.R;
+
+public class Part1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part1);
+    }
+
+}
