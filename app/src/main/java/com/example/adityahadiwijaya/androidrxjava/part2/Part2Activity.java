68cdcf71ca4c8b6c0778d9486d70a24bf8b4b0e9
Aditya Hadiwijaya <kulik2@ymail.com>
2016-12-16 17:53:07 +0700

    delete activity part 2 layout xml and change part2activity class content view to part 1

--- app/src/main/java/com/example/adityahadiwijaya/androidrxjava/part2/Part2Activity.java
+++ app/src/main/java/com/example/adityahadiwijaya/androidrxjava/part2/Part2Activity.java
@@ -3,11 +3,13 @@ package com.example.adityahadiwijaya.androidrxjava.part2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.adityahadiwijaya.androidrxjava.R;
+
 public class Part2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_part2);
+        setContentView(R.layout.activity_part1);
     }
 }
