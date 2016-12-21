f12458761e07a1b26c96ba7ba89559ef9adfe075
Aditya Hadiwijaya <kulik2@ymail.com>
2016-12-21 11:00:04 +0700

    create new part 4 detail activities

--- app/src/main/java/com/example/adityahadiwijaya/androidrxjava/part4/Part4DetailActivity.java
+++ app/src/main/java/com/example/adityahadiwijaya/androidrxjava/part4/Part4DetailActivity.java
@@ -1,5 +1,7 @@
 package com.example.adityahadiwijaya.androidrxjava.part4;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,27 +17,31 @@ import com.example.adityahadiwijaya.androidrxjava.part4.utils.OnRVClickListener;
  * Created by adityahadiwijaya on 12/19/16.
  */
 
-public class Part4DetailActivity extends AppCompatActivity implements OnRVClickListener {
+public class Part4DetailActivity extends AppCompatActivity{
 
     private final static String TAG = "Dodol";
+    private final static String USER_KEY = "user";
     private RecyclerView recyclerView;
+    private String requestedUser;
+
+    public static Intent from (Context context, String username){
+        Intent intent = new Intent(context, Part4DetailActivity.class);
+        intent.putExtra(USER_KEY, username);
+
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_part4);
+        setContentView(R.layout.activity_part4_detail);
 
-        GitHubUsersAdapter adapter = new GitHubUsersAdapter(this);
-        GitHubWrapper.getUsersInto(adapter);
-        recyclerView = (RecyclerView)findViewById(R.id.usersList);
+        requestedUser = getIntent().getStringExtra(USER_KEY);
+        setTitle(requestedUser);
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
-    }
 
-    @Override
-    public void onItemClicked(String username) {
-        Log.i(TAG, "onItemClicked: ");
+
     }
 }
